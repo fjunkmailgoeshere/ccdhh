ed71c249bf71fb23836e436dd56ade6572c85e77
Fun Project <forexjunkmailgoeshere@gmail.com>
2017-03-29 00:56:19 -0600

    Finished create new user page

--- client/config/router2.js
+++ client/config/router2.js
@@ -3,8 +3,31 @@ FlowRouter.route('/ccdhhAdmin', {
         BlazeLayout.render("mainLayout", {content: "ccdhhAdmin"});
     }
 });
+
 FlowRouter.route('/ccdhhCreateNewUser', {
     action: function() {
         BlazeLayout.render("mainLayout", {content: "ccdhhCreateNewUser"});
+    }
+});
+
+
+FlowRouter.route('/ccdhhEditUser', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhEditUser"});
+    }
+});
+FlowRouter.route('/ccdhhChangePassword', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhChangePassword"});
+    }
+});
+FlowRouter.route('/ccdhhBackupData', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhBackupData"});
+    }
+});
+FlowRouter.route('/ccdhhAdminApproval', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhAdminApproval"});
     }
 });
