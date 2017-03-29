cb6499584cf7a7f65e6eccba0ce93ab4caa2c8db
Katie Cue <katiecue@Katies-iMac.local>
2017-03-29 02:58:34 -0600

    created library, communication acceess and login folders. updated routes3.js. Updated navigation.html

--- client/config/routes3.js
+++ client/config/routes3.js
@@ -47,4 +47,97 @@ FlowRouter.route('/ccdhhQueryResults', {
 	action: function() {
 		BlazeLayout.render("mainLayout", {content: "ccdhhQueryResults"})
 	}
+});
+
+
+//
+// Library
+//
+
+FlowRouter.route('/ccdhhLibrary', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhLibrary"})
+	}
+});
+
+FlowRouter.route('/ccdhhTrainingMaterials', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhTrainingMaterials"})
+	}
+});
+
+FlowRouter.route('/ccdhhTrainingModules', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhTrainingModules"})
+	}
+});
+
+FlowRouter.route('/ccdhhTrainingAdmin', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhTrainingAdmin"})
+	}
+});
+
+FlowRouter.route('/ccdhhCreateTrainingModule', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhCreateTrainingModule"})
+	}
+});
+
+FlowRouter.route('/ccdhhEditTrainingSession', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhEditTrainingSession"})
+	}
+});
+
+FlowRouter.route('/ccdhhEditTrainingModule', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhEditTrainingModule"})
+	}
+});
+
+FlowRouter.route('/ccdhhViewSurveyResults', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhViewSurveyResults"})
+	}
+});
+
+
+//
+// Communication Access Assessment
+//
+
+FlowRouter.route('/ccdhhCommunicationAccess', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhCommunicationAccess"})
+	}
+});
+
+FlowRouter.route('/ccdhhAssessment', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhAssessment"})
+	}
+});
+
+
+//
+// Login Stuff
+//
+
+FlowRouter.route('/ccdhhforgotpassword', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhforgotpassword"})
+	}
+});
+
+FlowRouter.route('/ccdhhlockscreen', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhlockscreen"})
+	}
+});
+
+FlowRouter.route('/ccdhhregister', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhregister"})
+	}
 })
