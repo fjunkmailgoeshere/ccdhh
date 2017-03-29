f6adca344f1888c911411852ad0e1a9b23c2339c
Fun Project <forexjunkmailgoeshere@gmail.com>
2017-03-29 03:18:30 -0600

    Merged three router files

--- client/config/router.js
+++ client/config/router.js
@@ -29,6 +29,198 @@ FlowRouter.route('/ccdhhConsults', {
 });
 
 
+FlowRouter.route('/ccdhhAdmin', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhAdmin"});
+    }
+});
+
+FlowRouter.route('/ccdhhCreateNewUser', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhCreateNewUser"});
+    }
+});
+
+
+FlowRouter.route('/ccdhhEditUser', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhEditUser"});
+    }
+});
+FlowRouter.route('/ccdhhChangePassword', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhChangePassword"});
+    }
+});
+FlowRouter.route('/ccdhhBackupData', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhBackupData"});
+    }
+});
+FlowRouter.route('/ccdhhAdminApproval', {
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "ccdhhAdminApproval"});
+    }
+});
+
+
+//
+// Trainings
+//
+
+FlowRouter.route('/ccdhhTrainings', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhTrainings"})
+	}
+});
+
+FlowRouter.route('/ccdhhAddNewSession', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhAddNewSession"})
+	}
+});
+
+FlowRouter.route('/ccdhhUploadSurveyResults', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhUploadSurveyResults"})
+	}
+});
+
+FlowRouter.route('/ccdhhSurveyMode', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhSurveyMode"})
+	}
+});
+
+
+//
+// Reports Query
+//
+
+FlowRouter.route('/ccdhhReportsQuery', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhReportsQuery"})
+	}
+});
+
+FlowRouter.route('/ccdhhQueryPage', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhQueryPage"})
+	}
+});
+
+FlowRouter.route('/ccdhhQueryResults', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhQueryResults"})
+	}
+});
+
+
+//
+// Library
+//
+
+FlowRouter.route('/ccdhhLibrary', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhLibrary"})
+	}
+});
+
+FlowRouter.route('/ccdhhTrainingMaterials', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhTrainingMaterials"})
+	}
+});
+
+FlowRouter.route('/ccdhhTrainingModules', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhTrainingModules"})
+	}
+});
+
+FlowRouter.route('/ccdhhTrainingAdmin', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhTrainingAdmin"})
+	}
+});
+
+FlowRouter.route('/ccdhhCreateTrainingModule', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhCreateTrainingModule"})
+	}
+});
+
+FlowRouter.route('/ccdhhEditTrainingSession', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhEditTrainingSession"})
+	}
+});
+
+FlowRouter.route('/ccdhhEditTrainingModule', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhEditTrainingModule"})
+	}
+});
+
+FlowRouter.route('/ccdhhViewSurveyResults', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhViewSurveyResults"})
+	}
+});
+
+
+//
+// Communication Access Assessment
+//
+
+FlowRouter.route('/ccdhhCommunicationAccess', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhCommunicationAccess"})
+	}
+});
+
+FlowRouter.route('/ccdhhAssessment', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhAssessment"})
+	}
+});
+
+
+//
+// Login Stuff
+//
+
+FlowRouter.route('/ccdhhforgotpassword', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhforgotpassword"})
+	}
+});
+
+FlowRouter.route('/ccdhhlockscreen', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhlockscreen"})
+	}
+});
+
+FlowRouter.route('/ccdhhregister', {
+	action: function() {
+		BlazeLayout.render("mainLayout", {content: "ccdhhregister"})
+	}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
