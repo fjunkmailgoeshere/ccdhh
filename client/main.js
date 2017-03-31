91c43c3bf80f6906e5151fb94f6293d159a09894
Katie Cue <katiecue@Katies-MacBook-Air.local>
2017-03-30 18:13:55 -0600

    added server folder

--- client/main.js
+++ client/main.js
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
+//Consults = new Mongo.Collection('consults');
+
+Template.ccdhhConsults.events({
+	'submit form': function(event) {
+		event.preventDefault();
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
---
+++ server/main.js
@@ -0,0 +1,7 @@
+//import { Meteor } from 'meteor/meteor';
+
+Consults = new Mongo.Collection('consults');
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
