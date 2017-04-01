e5c7611a79e7c30f303d74311926ced3da5935d6
Fun Project <forexjunkmailgoeshere@gmail.com>
2017-04-01 16:41:14 -0600

    Fixed consults.html and consults.js to a clean, modular approach.  Consults is functional now but has more form elements to add.  Date picker works as well, except has formatting issues to fix.

--- client/views/pages/sendtoccdhh/consults/consults.js
+++ client/views/pages/sendtoccdhh/consults/consults.js
@@ -1,28 +1,6 @@
 
 /* code here */
-/*Template.ccdhhConsults.rendered=function() {
 
-// Initialize datapicker --- fix later
-
-/*
-	$('#data_1 .input-group.date').datepicker({
-    todayBtn: "linked",
-    keyboardNavigation: false,
-    forceParse: false,
-    calendarWeeks: true,
-    autoclose: true
-});
-*/
-	
- Initialize i-check plugin
-    $('.i-checks').iCheck({
-       checkboxClass: 'icheckbox_square-green',
-        radioClass: 'iradio_square-green'
-    });
-
-
-
-Consults = new Mongo.Collection('consults');
 
 Template.ccdhhConsults.events({
 	'submit form': function(event) {
@@ -30,88 +8,41 @@ Template.ccdhhConsults.events({
 
 		//Check the checkboxes for reasonforcontact
 		var reason = [];
-		alert(event);
+		if (event.target.reason_interpreters.checked == true) { 
+			reason.push(event.target.reason_interpreters.value);
+		}
+		if (event.target.reason_cart.checked == true) { 
+			reason.push(event.target.reason_cart.value);
+		}				
 
-		if (event.target.interpreters.checked == true) { 
-			reason.push(event.target.communicationaccess.value);
-		}
-		if (event.target.cart.checked == true) { 
-			reason.push(event.target.hearingassistivetechnology.value);
-		}
-		if (event.target.ssp.checked == true) { 
-			reason.push(event.target.cart.value);
-			
-		}
-		if (event.target.hearingassistivetechnology.checked == true) { 
-			reason.push(event.target.cart.value);
-			
-		}
-		if (event.target.telecommunicationsequipment.checked == true) { 
-			reason.push(event.target.cart.value);
-			
-		}
-		if (event.target.alertingdevices.checked == true) { 
-			reason.push(event.target.cart.value);
-		}
-		if (event.target.referral.checked == true) { 
-			reason.push(event.target.informationorreferral.value);
-			
-		}
-		if (event.target.research.checked == true) { 
-			reason.push(event.target.cart.value);
-			
-		}
-		if (event.target.websites.checked == true) { 
-			reason.push(event.target.cart.value);
-			
-		}
-		if (event.target.laws.checked == true) { 
-			reason.push(event.target.cart.value);
-			
-		}
-		if (event.target.informationonnavigatingsystems.checked == true) { 
-			reason.push(event.target.cart.value);
-			
-		}
-		if (event.target.lawenforcement.checked == true) { 
-			reason.push(event.target.cart.value);
-			
-		}
-		if (event.target.schools.checked == true) { 
-			reason.push(event.target.cart.value);
-			
-		}
-		if (event.target.workplace.checked == true) { 
-			reason.push(event.target.cart.value);
-		}
-		
-
-		// B/C I temporarily commented out all the stuff above, this will print an empty array to console
-		console.log(reason)
-
-
-		// All items commented out are commented out because there is no name="" for them in the HTML file.  (No equivalent representation in the HTML).  It's like saying "Go to San Francisco to get a slice of bread" but San Francisco doesn't exist.  So it's searching for something that doesn't exist, and crashes.  You can change the stuff here, or in the HTML file, but they both have to connect or the script won't work.  I've had them commented out so that this script can run.
-
+		//Put everything into a dictionary that will become the JSON object inserted into the DB
 		var data = {
-			date: event.target.dateofcontact.value,
-			name: event.target.personsname.value,
-			entity: event.target.entity.value,
-			hearingstatus: event.target.hearingstatus.value,
-			methodofcontact: event.target.methodofcontact.value,
-			reasonforcontact: event.target.reasonforcontact.value,
-			age: event.target.age.value,
+			date: event.target.date.value,
+			name: event.target.name.value,
 			gender: event.target.gender.value,
+			reasonforcontact: reason,
 			persontakingcall: event.target.persontakingcall.value,
-			resultofinquiry: event.target.resultofinquiry.value,
-			technicalassistancenotes: event.target.technicalassistancenotes.value
+			notes: event.target.notes.value
 		};
 
+
+		//Output the contents of what will be inserted into the DB for diagnostic purposes
+		//Remove on final deployment
 		console.log(data);
+
+		//Insert the data object into the DB
 		Consults.insert(data);
 
 	}
 	
 });
-consults.js
-Open with
-Displaying consults.js.
+
+Template.ccdhhConsults.rendered = function() {
+    $('#data_1 .input-group.date').datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true
+    });
+}
