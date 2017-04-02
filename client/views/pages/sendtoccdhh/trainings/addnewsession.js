b4afde95f8c31e3649c0500fbe234b8fa6f9910b
Katie Cue <katiecue@Katies-iMac.local>
2017-04-02 02:17:34 -0600

    review a2.html and js file for checkboxes error

--- client/views/pages/sendtoccdhh/trainings/addnewsession.js
+++ client/views/pages/sendtoccdhh/trainings/addnewsession.js
@@ -5,14 +5,32 @@ Template.ccdhhAddNewSession.events({
 	'submit form': function(event) {
 		event.preventDefault();
 		
+		//Checkboxes for Activity Type
+		var reason = [];
+		if (event.target.activity_activity1.checked == true) {
+			activity.push(event.target.activity_activity1.value);
+		}
+		if (event.target.activity_activity2.checked == true) {
+			activity.push(event.target.activity_activity2.value);
+		}
+		if (event.target.activity_activity3.checked == true) {
+			activity.push(event.target.activity_activity3.value);
+		}
+		if (event.target.activity_activity4.checked == true) {
+			activity.push(event.target.activity_activity4.value);
+		}
+		if (event.target.activity_activity5.checked == true) {
+			activity.push(event.target.activity_activity5.value);
+		}
 		
-//Put everything into a dictionary that will become the JSON object inserted into the DB
+		
+		//Put everything into a dictionary that will become the JSON object inserted into the DB
 		var data = {
 			modules: event.target.modules.value,
-			//dateoftraining: event.target.dateoftraining.value,
-			//titleoftraining: event.target.titleoftraining.value,
-			//numberofpeopleattended: event.target.numberofpeopleattended.value,
-			//activitytype: activitytype,
+			dateoftraining: event.target.dateoftraining.value,
+			titleoftraining: event.target.titleoftraining.value,
+			numberofattendees: event.target.numberofattendees.value,
+			activitytype: activity,
 			//locationoftraining: event.target.locationoftraining.value,
 			//county: event.target.county.value,
 			//lengthoftraining: event.target.lengthoftraining.value,
