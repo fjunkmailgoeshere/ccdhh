8464e685c077e5d4cd6ccfa82e73a134239718ad
Fun Project <forexjunkmailgoeshere@gmail.com>
2017-04-03 21:38:31 -0600

    Removed insecure, added some security on meteor.call server side. System now inserts DB and returns document inserted

--- server/main.js
+++ server/main.js
@@ -1,6 +1,32 @@
 //import { Meteor } from 'meteor/meteor';
 
 Consults = new Meteor.Collection('consults');
+Trainings = new Meteor.Collection('trainings');
+
+Meteor.methods({
+	'insertConsults': function(data) {
+		var currentUser = Meteor.userId();
+		if (currentUser) {
+			data["userId"] = currentUser;
+		} else {
+			throw new Meteor.Error("not-logged-in", "You're not logged-in.");
+		}
+		return Consults.insert(data);
+
+	},
+
+	'insertTrainings': function(data) {
+		var currentUser = Meteor.userId();
+		if (currentUser) {
+			data["userId"] = currentUser;
+		} else {
+			throw new Meteor.Error("not-logged-in", "You're not logged-in.");
+		}
+		return Trainings.insert(data);
+
+	}
+});
+
 
 Meteor.startup(() => {
   // code to run on server at startup
