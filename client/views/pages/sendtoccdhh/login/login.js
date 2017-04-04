239c1f717b1a4cb3b96f8b680d1249980eb19c79
Fun Project <forexjunkmailgoeshere@gmail.com>
2017-04-03 19:52:05 -0600

    Fixed changes to login.html so it is functioning again

--- client/views/pages/sendtoccdhh/login/login.js
+++ client/views/pages/sendtoccdhh/login/login.js
@@ -1,18 +1,17 @@
 Template.ccdhhLogin.events({
 	'submit form': function(event) {
 		event.preventDefault();
+		alert("called");
 
 		var email = $('[name=Username]').val();
 		var password = $('[name=Password]').val()
 
 		Meteor.loginWithPassword(email, password, function(error){
-		if(error){
-			alert(error.reason);
-		} else {
-			FlowRouter.go("/main");
-		}
-});
-
-
+			if(error){
+				alert(error.reason);
+			} else {
+				FlowRouter.go("/main");
+			}
+		});
 	}
 });
